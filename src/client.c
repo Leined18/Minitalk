907738627a7cdfd35c6c494516fb21c5a41d61d7
Leined18 <erdanielmarciano@hotmail.com>
2024-09-01 22:02:32 +0200

    new

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: danpalac <danpalac@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 20:22:57 by danpalac          #+#    #+#             */
-/*   Updated: 2024/09/01 20:48:18 by danpalac         ###   ########.fr       */
+/*   Updated: 2024/09/01 21:01:53 by danpalac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,8 @@
 
 int	main(int argc, char **argv)
 {
-	int pid;
 
 	if (argc != 3)
 		ft_error("", 1);
-	pid = ft_atoi(argv[2]);
+	ft_printf("%s", argv[3]);
 }
