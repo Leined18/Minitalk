af34f067b47ef895294c6c717035e3a24bd3336f
Leined18 <erdanielmarciano@hotmail.com>
2024-10-05 12:35:45 +0200

    me llegan datos

--- src/client.c
+++ src/client.c
@@ -1,6 +1,77 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danpalac <danpalac@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 08:23:00 by danpalac          #+#    #+#             */
+/*   Updated: 2024/10/05 12:32:53 by danpalac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main() {
-    printf("pepe\n");
-    return 0;
+#include "minitalk.h"
+
+static void	error(char *str, char *msg)
+{
+	if (str)
+		free(str);
+	ft_error(msg, 1);
+}
+
+static int	send_null(int pid, char *str)
+{
+	static int	i = 0;
+
+	if (i++ != 8)
+	{
+		if (kill(pid, SIGUSR1) == -1)
+			error(str, 0);
+		return (0);
+	}
+	free(str);
+	return (1);
+}
+
+static int	send_bit(int pid, char *str)
+{
+	static char	*message = 0;
+	static int	s_pid = 0;
+	static int	bits = -1;
+	int			byte_index;
+
+	if (str)
+		message = ft_strdup(str);
+	if (!message)
+		error(0, 0);
+	if (pid)
+		s_pid = pid;
+	byte_index = ++bits / 8;
+	if (byte_index < (int)ft_strlen(message) && message[byte_index])
+	{
+		if (message[byte_index] & (0x80 >> (bits % 8)))
+		{
+			if (kill(s_pid, SIGUSR2) == -1)
+				error(message, 0);
+		}
+		else if (kill(s_pid, SIGUSR1) == -1)
+			error(message, 0);
+		return (0);
+	}
+	return (send_null(s_pid, message));
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 3 || !ft_isstrnum(argv[1]))
+	{
+		ft_error("client: invalid arguments.\n", 0);
+		ft_putstr_color_fd(YELLOW, "correct format: [./client <PID> <STR>].\n",
+			2);
+		ft_error("", 1);
+	} 
+	send_bit(ft_atoi(argv[1]), argv[2]);
+	while (!send_bit(0, 0))
+		usleep(400);
+	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -1,6 +1,69 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danpalac <danpalac@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/11 16:39:51 by danpalac          #+#    #+#             */
+/*   Updated: 2024/10/05 12:17:26 by danpalac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main() {
-    printf("papa\n");
-    return 0;
+#include "minitalk.h"
+
+void	error(int pid, char *str)
+{
+	if (str)
+		free(str);
+	kill(pid, SIGUSR2);
+	ft_error("server: unexpected error.", 1);
+}
+
+char	*print_and_free(char *message)
+{
+	ft_putstr_fd(message, 1);
+	free(message);
+	return (NULL);
+}
+
+void	handler(int signum)
+{
+	static char	c = 0xFF;
+	static int	bits = 0;
+	static char	*message = 0;
+    
+	if (signum == SIGUSR1)
+		c ^= 0x80 >> bits;
+	else if (signum == SIGUSR2)
+		c |= 0x80 >> bits;
+	if (++bits == 8)
+	{
+		if (c)
+			message = ft_straddc(message, c);
+		else
+			message = print_and_free(message);
+		bits = 0;
+		c = 0xFF;
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
+	sigset_t			b_mask;
+
+	sigemptyset(&b_mask);
+	sigaddset(&b_mask, SIGINT);
+	sigaddset(&b_mask, SIGQUIT);
+	sa.sa_handler = handler;
+	sa.sa_mask = b_mask;
+	sa.sa_flags = 0;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	ft_putstr_color_fd(GREEN, "PID: ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putstr_fd("\n", 1);
+	while (1)
+		pause();
 }
